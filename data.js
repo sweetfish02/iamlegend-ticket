83f8c85a8e39970d575990c784fb96398f3d0851
sweetfish02 <choijaeyeong0213@gmail.com>
2025-11-21 22:31:55 +0900

    Add files via upload
    Signed-off-by: sweetfish02 <choijaeyeong0213@gmail.com>

---
+++ data.js
@@ -0,0 +1,292 @@
+const rewardData = {
+  "3_80": [
+    {
+      "name": "최종보상",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "A",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "B",
+      "count": 2,
+      "price": 270,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "C",
+      "count": 3,
+      "price": 180,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "D",
+      "count": 4,
+      "price": 135,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "E",
+      "count": 20,
+      "price": 27,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "F",
+      "count": 20,
+      "price": 27,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "G",
+      "count": 30,
+      "price": 18,
+      "score": 540,
+      "ticket": 18
+    }
+  ],
+  "2_240": [
+    {
+      "name": "최종보상",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "A",
+      "count": 2,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "B",
+      "count": 4,
+      "price": 270,
+      "score": 270,
+      "ticket": 9
+    },
+    {
+      "name": "C",
+      "count": 9,
+      "price": 120,
+      "score": 360,
+      "ticket": 12
+    },
+    {
+      "name": "D",
+      "count": 15,
+      "price": 72,
+      "score": 288,
+      "ticket": 9.6
+    },
+    {
+      "name": "E",
+      "count": 30,
+      "price": 36,
+      "score": 144,
+      "ticket": 4.8
+    },
+    {
+      "name": "F",
+      "count": 33,
+      "price": 12,
+      "score": 396,
+      "ticket": 13.2
+    },
+    {
+      "name": "G",
+      "count": 90,
+      "price": 12,
+      "score": 312,
+      "ticket": 10.4
+    }
+  ],
+  "2_80": [
+    {
+      "name": "최종보상",
+      "count": 1,
+      "price": 360,
+      "score": 360,
+      "ticket": 12
+    },
+    {
+      "name": "A",
+      "count": 1,
+      "price": 360,
+      "score": 0,
+      "ticket": 0
+    },
+    {
+      "name": "B",
+      "count": 2,
+      "price": 180,
+      "score": 180,
+      "ticket": 6
+    },
+    {
+      "name": "C",
+      "count": 3,
+      "price": 120,
+      "score": 120,
+      "ticket": 4
+    },
+    {
+      "name": "D",
+      "count": 4,
+      "price": 90,
+      "score": 90,
+      "ticket": 3
+    },
+    {
+      "name": "E",
+      "count": 20,
+      "price": 18,
+      "score": 90,
+      "ticket": 3
+    },
+    {
+      "name": "F",
+      "count": 20,
+      "price": 18,
+      "score": 54,
+      "ticket": 1.8
+    },
+    {
+      "name": "G",
+      "count": 30,
+      "price": 12,
+      "score": 60,
+      "ticket": 2
+    }
+  ],
+  "2_120": [
+    {
+      "name": "최종보상",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "A",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "B",
+      "count": 3,
+      "price": 180,
+      "score": 0,
+      "ticket": 0
+    },
+    {
+      "name": "C",
+      "count": 6,
+      "price": 90,
+      "score": 270,
+      "ticket": 9
+    },
+    {
+      "name": "D",
+      "count": 10,
+      "price": 54,
+      "score": 324,
+      "ticket": 10.8
+    },
+    {
+      "name": "E",
+      "count": 30,
+      "price": 18,
+      "score": 306,
+      "ticket": 10.2
+    },
+    {
+      "name": "F",
+      "count": 30,
+      "price": 18,
+      "score": 288,
+      "ticket": 9.6
+    },
+    {
+      "name": "G",
+      "count": 40,
+      "price": 12,
+      "score": 288,
+      "ticket": 9.6
+    }
+  ],
+  "3_160": [
+    {
+      "name": "최종보상",
+      "count": 1,
+      "price": 540,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "A",
+      "count": 2,
+      "price": 540,
+      "score": 0,
+      "ticket": 0
+    },
+    {
+      "name": "B",
+      "count": 3,
+      "price": 360,
+      "score": 1080,
+      "ticket": 36
+    },
+    {
+      "name": "C",
+      "count": 6,
+      "price": 180,
+      "score": 1080,
+      "ticket": 36
+    },
+    {
+      "name": "D",
+      "count": 9,
+      "price": 120,
+      "score": 600,
+      "ticket": 20
+    },
+    {
+      "name": "E",
+      "count": 40,
+      "price": 27,
+      "score": 621,
+      "ticket": 20.7
+    },
+    {
+      "name": "F",
+      "count": 40,
+      "price": 27,
+      "score": 540,
+      "ticket": 18
+    },
+    {
+      "name": "G",
+      "count": 60,
+      "price": 18,
+      "score": 198,
+      "ticket": 6.6
+    }
+  ]
+};
